f65e10f1f5e469c8666617400cb8257a0885c693
Cristiano Leite <cristianosleite@hotmail.com>
2023-02-21 16:51:15 -0300

    Delete unused directive

--- src/app/login-page/registration.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RegistrationDirective } from './registration.directive';
-
-describe('RegistrationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RegistrationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
