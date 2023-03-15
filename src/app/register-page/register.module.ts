152350d24bb7d6edd74f49e7528cc0a205596f66
Cristiano Leite <cristianosleite@hotmail.com>
2023-03-14 21:42:52 -0300

    Update register.module.ts

--- src/app/register-page/register.module.ts
+++ src/app/register-page/register.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RegisterPage} from './register.page';
-
 import { RegisterPageRoutingModule } from './register-routing.module';
 
 @NgModule({
