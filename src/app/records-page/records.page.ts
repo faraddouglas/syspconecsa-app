81c08f6042f97b953ed05283fcb18839ea13f64f
Cristiano Leite <cristianosleite@hotmail.com>
2023-04-16 14:05:44 -0300

    Implemented banked hours

--- src/app/records-page/records.page.ts
+++ src/app/records-page/records.page.ts
@@ -9,9 +9,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RecordsPage implements OnInit {
   constructor(private recordService: RecordService) {}
-  records: object = [];
+  records: any = [];
   user: any = localStorage.getItem('user');
   hasInterval = JSON.parse(this.user).hasInterval;
+  bankedHours: any [] = [];
+  extraHours: any [] = [];
+  weekendHours: any [] = [];
+  totalBankedHours: string = '00:00';
+  totalExtraHours: string = '00:00';
+  totalWeekendHours: string = '00:00';
 
   ngOnInit() {
     this.loadRecords();
@@ -19,24 +25,61 @@ export class RecordsPage implements OnInit {
 
   async loadRecords(){
     this.records = await this.recordService.getRecords();
+
+    // Get banked hours
+    const workedHours = this.records.map((record: any) => record.bankedHours);
+    for (let i = 0; i < workedHours.length; i++) {
+      if (workedHours.length > 0 && workedHours[i] !== undefined) {
+        this.bankedHours.push(workedHours[i]['totalWorkedHoursWithInterval']);
+        this.extraHours.push(workedHours[i]['extraHours']);
+        this.weekendHours.push(workedHours[i]['extraWeekendHours']);
+      };
+    };
+    this.totalBankedHours = this.sumTimeValues(this.bankedHours);
+    this.totalExtraHours = this.sumTimeValues(this.extraHours);
+    this.totalWeekendHours = this.sumTimeValues(this.weekendHours);
   };
 
   formatDate(_date: string) {
-
     const date = new Date(_date);
-    const day = date.getUTCDate();
-    const month = date.getUTCMonth() + 1;
-    const year = date.getUTCFullYear();
+    const day = this.padLeadingZero(date.getDate());
+    const month = this.padLeadingZero(date.getMonth() + 1);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
 
-    if(day < 10 && month < 10){
-      return `0${day}/0${month}/${year}`;
-    } else if(day < 10 && month >= 10){
-      return `0${day}/${month}/${year}`;
-    } else if(day >= 10 && month < 10){
-      return `${day}/0${month}/${year}`;
-    } else {
-      return `${day}/${month}/${year}`;
-    };
+  formatTime(_time: string) {
+    const time = new Date(_time);
+    const hours = this.padLeadingZero(time.getUTCHours());
+    const minutes = this.padLeadingZero(time.getUTCMinutes());
+    const seconds = this.padLeadingZero(time.getUTCSeconds());
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  
+  padLeadingZero(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }  
+
+  sumTimeValues(timeValues: string[]): string {
+    let totalHours = 0;
+    let totalMinutes = 0;
+  
+    // Loop through each time value
+    timeValues.forEach(timeValue => {
+      const [hours, minutes] = timeValue.split(':').map(Number);
+      totalHours += hours;
+      totalMinutes += minutes;
+    });
+  
+    // Convert excess minutes to hours
+    totalHours += Math.floor(totalMinutes / 60);
+    totalMinutes %= 60;
+  
+    // Format the total hours and minutes as "hh:mm"
+    const formattedHours = totalHours < 10 ? `0${totalHours}` : `${totalHours}`;
+    const formattedMinutes = totalMinutes < 10 ? `0${totalMinutes}` : `${totalMinutes}`;
+  
+    return `${formattedHours}:${formattedMinutes}`;
   };
 };
 
