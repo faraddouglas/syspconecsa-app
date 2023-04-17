0be7c4b76e30186420a4604888605c8d4bd43353
Cristiano Leite <cristianosleite@hotmail.com>
2023-04-16 23:58:45 -0300

    Implemented records filter

--- src/app/records-page/records.page.ts
+++ src/app/records-page/records.page.ts
@@ -8,7 +8,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   styleUrls: ['records.page.scss']
 })
 export class RecordsPage implements OnInit {
-  constructor(private recordService: RecordService) {}
+  constructor(
+    private recordService: RecordService
+  ) {}
+
   records: any = [];
   user: any = localStorage.getItem('user');
   hasInterval = JSON.parse(this.user).hasInterval;
@@ -18,16 +21,67 @@ export class RecordsPage implements OnInit {
   totalBankedHours: string = '00:00';
   totalExtraHours: string = '00:00';
   totalWeekendHours: string = '00:00';
-
-  ngOnInit() {
-    this.loadRecords();
+  selectedInterval = 'week'
+  interval = {
+    startDate: '',
+    endDate: ''
   };
 
-  async loadRecords(){
-    this.records = await this.recordService.getRecords();
+  ngOnInit() {
+    this.getRecordsByInterval(this.selectedInterval);
+  };
 
-    // Get banked hours
-    const workedHours = this.records.map((record: any) => record.bankedHours);
+  async getRecordsByInterval(interval: string) {
+    this.records = await this.recordService.getRecords();
+  
+    // Filter records based on interval
+    const currentDate = new Date();
+    let startDate: Date;
+    switch (interval) {
+      case 'week':
+        startDate = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+        this.records = this.records.filter((record: { date: string | number | Date; }) => {
+          const recordDate = new Date(record.date);
+          return this.formatDate(recordDate) >= this.formatDate(startDate) && 
+            this.formatDate(recordDate) <= this.formatDate(currentDate);
+        });
+        break;
+      case 'month':
+        startDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate());
+        this.records = this.records.filter((record: { date: string | number | Date; }) => {
+          const recordDate = new Date(record.date);
+          return this.formatDate(recordDate) >= this.formatDate(startDate) && 
+            this.formatDate(recordDate) <= new Date(currentDate.getFullYear(), 
+            currentDate.getMonth(), 1).toDateString();
+        });
+        break;
+      case 'year':
+        startDate = new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate() + 1);
+        this.records = this.records.filter((record: { date: string | number | Date; }) => {
+          const recordDate = new Date(record.date);
+          return recordDate >= startDate && 
+            recordDate <= currentDate;
+        });
+        break;
+      // If interval is not 'week', 'month', or 'year', assume it's a specific date range
+      default:
+        startDate = new Date(this.interval.startDate);
+        const endDate = new Date(this.interval.endDate);
+        this.records = this.records.filter((record: { date: string | number | Date; }) => {
+          const recordDate = new Date(record.date);
+          return this.formatDate(recordDate) >= this.formatDate(startDate) && 
+            this.formatDate(recordDate) <= this.formatDate(endDate);
+        });
+        break;
+    };
+    this.getBankedHours();
+  };
+
+  async getBankedHours() {
+    this.bankedHours = [];
+    this.extraHours = [];
+    this.weekendHours = [];
+    const workedHours = await this.records.map((record: any) => record.bankedHours);
     for (let i = 0; i < workedHours.length; i++) {
       if (workedHours.length > 0 && workedHours[i] !== undefined) {
         this.bankedHours.push(workedHours[i]['totalWorkedHoursWithInterval']);
@@ -40,7 +94,7 @@ export class RecordsPage implements OnInit {
     this.totalWeekendHours = this.sumTimeValues(this.weekendHours);
   };
 
-  formatDate(_date: string) {
+  formatDate(_date: string | number | Date) {
     const date = new Date(_date);
     const day = this.padLeadingZero(date.getDate());
     const month = this.padLeadingZero(date.getMonth() + 1);
@@ -54,11 +108,11 @@ export class RecordsPage implements OnInit {
     const minutes = this.padLeadingZero(time.getUTCMinutes());
     const seconds = this.padLeadingZero(time.getUTCSeconds());
     return `${hours}:${minutes}:${seconds}`;
-  }
+  };
   
   padLeadingZero(value: number): string {
     return value < 10 ? `0${value}` : `${value}`;
-  }  
+  };
 
   sumTimeValues(timeValues: string[]): string {
     let totalHours = 0;
