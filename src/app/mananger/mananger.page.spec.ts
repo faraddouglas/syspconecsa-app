9d9df3e9b146e098608ac1c695b120fa821309f3
Cristiano Leite <cristianosleite@hotmail.com>
2023-04-21 19:36:01 -0300

    Update mananger.page.spec.ts

--- src/app/mananger/mananger.page.spec.ts
+++ src/app/mananger/mananger.page.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { ManangerPage } from './mananger.page';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CustomComponent } from '../custom-component/custom-component.component';
 
 describe('ManangerPage', () => {
   let component: ManangerPage;
@@ -10,7 +11,11 @@ describe('ManangerPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ManangerPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        HttpClientTestingModule,
+        CustomComponent
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ManangerPage);
