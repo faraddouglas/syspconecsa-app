77fac339355a6ace4f9d3c6031258fc84778f38f
Cristiano Leite <cristianosleite@hotmail.com>
2023-04-21 19:35:06 -0300

    Update auth.guard.spec.ts

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthGuard } from './auth.guard';
+import { CustomComponent } from '../custom-component/custom-component.component';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        CustomComponent
+      ]
+    });
     guard = TestBed.inject(AuthGuard);
   });
 
