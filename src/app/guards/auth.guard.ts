25882755237c209e465f4365600a198e83af6205
Cristiano Leite <cristianosleite@hotmail.com>
2023-05-11 12:33:11 -0300

    Formated code

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,106 +1,125 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { catchError, Observable } from 'rxjs';
 import { NavController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { CustomComponent } from '../custom-component/custom-component.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard {
   constructor(
     private navCtrl: NavController,
     private http: HttpClient,
     private customComponent: CustomComponent
-    ) {};
+  ) {}
 
   canActivate(
     _route: ActivatedRouteSnapshot,
-    _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    _state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     return this.checkAuth();
-  };
+  }
 
-  private async checkAuth(){
+  private async checkAuth() {
     const user = localStorage.getItem('user');
     const enterprise = localStorage.getItem('enterprise');
-    if(user || user !== 'undefined' || enterprise || enterprise !== 'undefined'){
-      return true
+    if (
+      user ||
+      user !== 'undefined' ||
+      enterprise ||
+      enterprise !== 'undefined'
+    ) {
+      return true;
     } else {
       return true || this.routToLogin();
-    };
-  };
+    }
+  }
 
-  private routToLogin(){
+  private routToLogin() {
     this.navCtrl.navigateRoot('page/login');
     return true;
-  };
+  }
 
-  logout(){
+  logout() {
     localStorage.removeItem('user');
     localStorage.removeItem('records');
     localStorage.removeItem('token');
     localStorage.removeItem('enterprise');
     this.routToLogin();
-  };
+  }
 
-  async login(companyId: any, userId: string){
-    const req: any =  this.http.post(
-      `https://syspteste.herokuapp.com/api/login?companyId=${companyId}&userId=${userId}`, {
-        responseType: 'json'
-      })
+  async login(companyId: any, userId: string) {
+    const req: any = this.http
+      .post(
+        `https://syspteste.herokuapp.com/api/login?companyId=${companyId}&userId=${userId}`,
+        {
+          responseType: 'json',
+        }
+      )
       .pipe(
         catchError((err) => {
-          if(err.status === 401){
+          if (err.status === 401) {
             this.customComponent.presentAlert(
               'Atenção!',
               'Não foi possível realizar o login!',
               'Verifique suas credenciais',
               ['OK']
-              );
+            );
             this.logout();
-          };
+          }
           throw new Error('Não foi possível realizar o login');
-        }
-      ));
-      req.subscribe((res: any) => {
-      if(res.user.userId === userId){
+        })
+      );
+    req.subscribe((res: any) => {
+      if (res.user.userId === userId) {
         localStorage.setItem('user', JSON.stringify(res.user));
         localStorage.setItem('token', res.token);
-          if(res.user.userType === 'admin'){
+        if (res.user.userType === 'admin') {
           this.navCtrl.navigateRoot('page/mananger');
         } else {
           this.navCtrl.navigateRoot('page/tabs/register');
-        };
-      };
-    });
-  };
-
-  async getEnterprise(companyId: string){
-    const req: any = this.http.post(
-      `https://syspteste.herokuapp.com/api/login/loginEnterprise?companyId=${companyId}`, {
-      responseType: 'json'
+        }
       }
-    )
-    .pipe(
-      catchError((err) => {
-        if(err.status === 401){
-          this.customComponent.presentAlert(
-            'Atenção!',
-            'Não foi possível realizar o login!',
-            'Empresa não cadastrada',
-            ['OK']
-            );
-          this.logout();
-          throw new Error('Não foi possível realizar o login');
-        };
-        throw new Error('Não foi possível realizar o login');
-      })
-    );
-    req.subscribe((res: any) => {
-      if(res.enterprise.companyId === companyId){
-        localStorage.setItem('enterprise', JSON.stringify(res.enterprise));
-      };
     });
-  };
-};
+  }
+
+  async getEnterprise(companyId: string) {
+    const req: any = this.http
+      .post(
+        `https://syspteste.herokuapp.com/api/login/loginEnterprise?companyId=${companyId}`,
+        {
+          responseType: 'json',
+        }
+      )
+      .pipe(
+        catchError((err) => {
+          if (err.status === 401) {
+            this.customComponent.presentAlert(
+              'Atenção!',
+              'Não foi possível realizar o login!',
+              'Empresa não cadastrada',
+              ['OK']
+            );
+            this.logout();
+            throw new Error('Não foi possível realizar o login');
+          }
+          throw new Error('Não foi possível realizar o login');
+        })
+      );
+    req.subscribe((res: any) => {
+      if (res.enterprise.companyId === companyId) {
+        localStorage.setItem('enterprise', JSON.stringify(res.enterprise));
+      }
+    });
+  }
+}
